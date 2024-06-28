f4a1c5eb04a3ffb809a76352143520c6332c6f44
ADMIN <juanmg777vg@gmail.com>
2024-06-28 10:13:32 -0500

    Juan no sabe hacer commits

---
+++ app/src/main/java/com/juandelgado11l/playground/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.juandelgado11l.playground
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.juandelgado11l.playground.ui.theme.PlaygroundTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PlaygroundTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/juandelgado11l/playground/ui/screens/Logincreen.kt
@@ -0,0 +1,82 @@
+package com.juandelgado11l.playground.ui.screens
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.juandelgado11l.playground.R
+import com.juandelgado11l.playground.ui.theme.PlaygroundTheme
+
+@Composable
+
+fun LoginScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            modifier = Modifier.size(250.dp),
+            painter = painterResource(id = R.drawable.login_image),
+            contentDescription = "Imagen del login"
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(text = "¡HOLA!", fontSize = 35.sp, fontWeight = FontWeight.Bold)
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(
+            modifier = Modifier.padding(horizontal = 50.dp),
+            textAlign = TextAlign.Center,
+            text = "¡HOLA! Descubre un mundo de posibilidades en Playground.Explora, conecta y disfruta. ¡Registrate ya!"
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(modifier = Modifier.width(250.dp),
+            colors = ButtonDefaults.buttonColors(Color.Black),
+            onClick = { /*TODO*/ }) {
+            Text(text = "Iniciar sesión")
+        }
+
+        Button(
+            modifier = Modifier.width(250.dp),
+            border = BorderStroke(1.dp, Color.Black),
+            colors = ButtonDefaults.outlinedButtonColors(),
+            onClick = { /*TODO*/ }) {
+            Text(text = "Registrarse", color = Color.Black)
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+
+    PlaygroundTheme {
+        LoginScreen()
+    }
+}
+
+
+
